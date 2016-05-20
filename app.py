1009730deb84f6cddef97f09f098329a6c0c3f1f
Neha <inehayadav28@gmail.com>
2016-05-21 00:33:30 +0530

    extension added

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+import os
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug import secure_filename, SharedDataMiddleware
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/upload')
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-   return render_template('upload.html')
-
-@app.route('/uploader', methods=['GET', 'POST'])
-def uploader_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return render_template('upload.html')
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename = filename))
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+   return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run()

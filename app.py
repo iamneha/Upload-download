a4d0a2700fa97f46718d0ccaa119c3af41db145d
Neha <inehayadav28@gmail.com>
2016-05-22 20:40:07 +0530

    Download

--- app.py
+++ app.py
@@ -31,5 +31,10 @@ def upload_file():
 def uploaded_file(filename):
    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST']) 
+def download(filename):
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER']) 
+    return send_from_directory(directory=uploads, filename = filename)
+
 if __name__ == '__main__':
     app.run()
